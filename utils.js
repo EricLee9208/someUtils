0ced2e963f4cb14508f698e4cb875cc795237c65
ethanol <ethan.gard@jane.app>
2022-09-15 18:25:31 -0400

    modified the add function

--- utils.js
+++ utils.js
@@ -1,15 +1,19 @@
-function product(numA, numB){
-    return numA * numB
+function product(numA, numB) {
+  return numA * numB;
 }
 
-function divide (num1, num2){
-    return num1/num2
+function divide(num1, num2) {
+  return num1 / num2;
 }
 
-function power (num1, num2){
-    return num1**num2
+function power(num1, num2) {
+  return num1 ** num2;
 }
 
-function subtract (num1, num2){
-    return num1-num2
+function subtract(num1, num2) {
+  return num1 - num2;
 }
+
+const add = (num1, num2) => {
+  return num1 + num2;
+};
